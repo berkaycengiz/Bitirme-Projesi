f0ff8cda047e3f4344ad3a52f5d2aed35ab0d6ee
Berkay Cengiz <berkaycengiz12321@gmail.com>
2026-03-02 20:44:35 +0300

    Client kurulumu ve theme eklendi.

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+  <BrowserRouter>
+    <Routes>
+      {/* <Route path="/" element={<Dashboard />} /> */}
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
